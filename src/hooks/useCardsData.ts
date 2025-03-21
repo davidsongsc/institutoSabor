1445b8cd7fb6b037a0aa731a6f6dbb3b970549e8
Davidson George Silverio Correa <davidson@verticalmkt.com.br>
2025-03-21 09:25:00 -0300

    Adição hook google sheets

---
+++ src/hooks/useCardsData.ts
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { App } from "antd";
+import Papa from "papaparse";
+
+export interface ListaItem {
+  texto: string;
+  img: string;
+}
+
+export interface CardData {
+  id: number;
+  title: string;
+  description: string;
+  lista: ListaItem[];
+}
+
+export function useCardsData(csvUrl: string) {
+  const [cards, setCards] = useState<CardData[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const { notification } = App.useApp();
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const res = await fetch(csvUrl);
+        const csvText = await res.text();
+
+        const parsed = Papa.parse(csvText, {
+          header: true,
+          skipEmptyLines: true,
+        });
+
+        const rows = parsed.data as any[];
+
+        const formatted: CardData[] = rows.map((row) => {
+          return {
+            id: Number(row["ID"]),
+            title: row["Título"],
+            description: row["Descrição"],
+            lista: row["Lista (JSON)"]
+              ? JSON.parse(row["Lista (JSON)"].replace(/""/g, '"'))
+              : [],
+          };
+        });
+
+        setCards(formatted);
+      } catch (err) {
+        console.error("Erro ao buscar ou processar CSV:", err);
+        notification.error({
+          message: "Erro",
+          description: "Não foi possível carregar os cards via CSV.",
+        });
+        setError("Erro ao carregar CSV");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCards();
+  }, [csvUrl]);
+
+  return { cards, loading, error };
+}
--- src/templates/homepage.tsx
+++ src/templates/homepage.tsx
@@ -1,27 +1,21 @@
 'use client';
 
-import { Button } from "antd";
+import { useCardsData } from "@/hooks/useCardsData";
+import { Button, Spin } from "antd";
 import Image from "next/image";
 import { useState } from "react";
 export default function HomePage() {
     const [activeIndex, setActiveIndex] = useState(1);
+    const CSV_URL =
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vRb4Gfi3HjunpPb_-tMBmwYNfrfDQffiY1oZheCBrITYr3SV4oZ8aiVTprLoTOh5_gntnjl3U2gRNre/pub?output=csv";
 
-    const cardsData = [
-        {
-            id: 1, title: "Chef Completo", description: "A Escolha ideal para quem deseja se tornar um chef versátil e altamente qualificado.", lista: [{ texto: "Fundamentos da gastronomia", img: "apostila" },
-            { texto: "Cozinha clássica internacional", img: "preparo" },
-            { texto: "Cozinha clássica brasileira", img: "cozido" },
-            { texto: "Cursos EAD e presencial", img: "monitor" },
-            { texto: "Carga horária: 360 Horas", img: "relogio" },
-            { texto: "Certificado de conclusão profissionalizante", img: "certificado" },
-            ]
-        },
-        { id: 2, title: "Gastronomia Brasileira", description: "Oferece uma imersão completa na riqueza e diversidade da culinária nacional.", lista: [{ texto: "Fundamentos da gastronomia", img: "apostila" }] },
-        { id: 3, title: "Cozinha moderna e inovadora", description: "Explore a gastronomia com uma abordagem dinâmica.", lista: [{ texto: "Fundamentos da gastronomia", img: "apostila" }] },
-        { id: 4, title: "Administração de restaurantes", description: "Preparar profissionais para a gestão eficiente de negócios gastronômicos.", lista: [{ texto: "Fundamentos da gastronomia", img: "apostila" }] },
+    const { cards, loading, error } = useCardsData(CSV_URL);
 
 
-    ];
+
+
+
+
     return (
         <div className="">
             <div className="grid grid-cols-12 gap-8 items-start justify-start h-[755px] ">
@@ -114,67 +108,67 @@ export default function HomePage() {
                     <h1 className="uppercase text-7xl relative z-10 font-[800]" >Conheça nossos <span className="text-primary">combos exclusivos</span></h1>
                 </div>
             </div>
-
-            <div className="grid grid-cols-12 items-start justify-center h-screen gap-4">
-                <span className="col-span-2"></span>
-                {cardsData.map((card, index) => (
-                    <div
-                        key={card.id}
-                        onMouseEnter={() => setActiveIndex(index)}
-                        className={`col-span-2 relative transition-all duration-500 ease-in-out ${activeIndex === index ? "group" : ""
-                            }`}
-                    >
+            <Spin spinning={loading} tip={<h1 className="text-7xl h-[100px] m-auto">Carregando</h1>} >
+                <div className="grid grid-cols-12 items-start justify-center h-screen gap-4">
+                    <span className="col-span-2"></span>
+                    {cards.map((card, index) => (
                         <div
-                            className={`h-[600px] ${activeIndex === index ? "h-[670px] translate-y-0" : "translate-y-[50px]"
-                                } transform rounded-[20px] bg-quinary transition-all duration-500 ease-in-out`}
+                            key={card.id}
+                            onMouseEnter={() => setActiveIndex(index)}
+                            className={`col-span-2 relative transition-all duration-500 ease-in-out ${activeIndex === index ? "group" : ""
+                                }`}
                         >
-                            <div className="relative overflow-hidden flex flex-col items-center rounded-[20px] border border-black h-full">
-                                <div
-                                    className={` w-full flex flex-col items-center justify-start rounded-[20px]
+                            <div
+                                className={`h-[600px] ${activeIndex === index ? "h-[670px] translate-y-0" : "translate-y-[50px]"
+                                    } transform rounded-[20px] bg-quinary transition-all duration-500 ease-in-out`}
+                            >
+                                <div className="relative overflow-hidden flex flex-col items-center rounded-[20px] border border-black h-full">
+                                    <div
+                                        className={` w-full flex flex-col items-center justify-start rounded-[20px]
                                             transition-all duration-300 px-2 py-8 
                                             ${activeIndex === index
-                                            ? "text-secondary bg-primary h-[250px]"
-                                            : "group-hover:text-secondary group-hover:bg-primary h-[220px]"
-                                        }`}
-                                >
-                                    <h3 className="text-2xl text-center uppercase font-[800] border-b-2 border-senary w-[280px] mb-2">
-                                        {card.title}
-                                    </h3>
-                                    <p className="text-sm text-center">{card.description}</p>
-                                    <Button
-                                        type="primary"
-                                        className={`mt-5 px-5 py-8 uppercase text-6xl ${activeIndex === index ? "bg-quaternary" : "group-hover:bg-quaternary"
+                                                ? "text-secondary bg-primary h-[250px]"
+                                                : "group-hover:text-secondary group-hover:bg-primary h-[220px]"
                                             }`}
                                     >
-                                        Saiba mais
-                                    </Button>
-                                </div>
+                                        <h3 className="text-2xl text-center uppercase font-[800] border-b-2 border-senary w-[280px] mb-2">
+                                            {card.title}
+                                        </h3>
+                                        <p className="text-sm text-center">{card.description}</p>
+                                        <Button
+                                            type="primary"
+                                            className={`mt-5 px-5 py-8 uppercase text-6xl ${activeIndex === index ? "bg-quaternary" : "group-hover:bg-quaternary"
+                                                }`}
+                                        >
+                                            Saiba mais
+                                        </Button>
+                                    </div>
 
-                                <div className="w-full bg-gray-100 flex items-start justify-center mt-4">
-                                    <ul className={` 
+                                    <div className="w-full bg-gray-100 flex items-start justify-center mt-4">
+                                        <ul className={` 
                                             ${activeIndex === index
-                                            ? "mt-4"
-                                            : "mt-0"
-                                        }`}>
-                                        {card.lista.map((item, index) => (
-                                            <li
-                                                key={index}
-                                                className="text-sm text-center font-bold mb-2"
-                                            >
-                                                <div className="flex items-center justify-start w-full">
-                                                    <Image src={`/images/items/${item.img}.webp`} alt="logo" width={70} height={100} ></Image>
-                                                    <span className="text-left text-[16px] w-[200px]">{item.texto}</span>
-                                                </div>
-                                            </li>
-                                        ))}
-                                    </ul>
+                                                ? "mt-4"
+                                                : "mt-0"
+                                            }`}>
+                                            {card.lista.map((item, index) => (
+                                                <li
+                                                    key={index}
+                                                    className="text-sm text-center font-bold mb-2"
+                                                >
+                                                    <div className="flex items-center justify-start w-full">
+                                                        <Image src={`/images/items/${item.img}.webp`} alt="logo" width={70} height={100} ></Image>
+                                                        <span className="text-left text-[16px] w-[200px]">{item.texto}</span>
+                                                    </div>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-
+                    ))}
+                </div>
+            </Spin>
 
 
         </div >
