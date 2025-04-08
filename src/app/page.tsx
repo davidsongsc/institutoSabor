42b1da88267f3f03acc96f290d2176d72ccfd052
Davidson George Silverio Correa <davidson@verticalmkt.com.br>
2025-04-08 11:43:05 -0300

    Implementação LazyComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { Spin } from "antd";
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from "react";
-
+import SejaBemVindo from "@/components/sections/homepage/bemvindo";
 export default function HomeNative() {
   const [showPage, setShowPage] = useState(false);
   const router = useRouter();
@@ -11,12 +11,12 @@ export default function HomeNative() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowPage(true);
-    }, 2000);
+    }, 500);
 
     return () => clearTimeout(timer);
   }, []);
 
-  if (!showPage) return <Spin className="h-screen w-screen flex justify-center items-center" size="large" tip="Carregando..." />
+  if (!showPage) { return <Spin className="h-screen w-screen flex justify-center items-center" size="large" ></Spin> }
   else (router.push(`/home`));
 
   return;
--- src/templates/homepage.tsx
+++ src/templates/homepage.tsx
@@ -1,36 +1,58 @@
 'use client';
 
 import './styles.css';
-import React, { useState } from "react";
+import React, { Suspense } from "react";
+import dynamic from "next/dynamic";
+import { useInView } from 'react-intersection-observer';
 import SejaBemVindo from "@/components/sections/homepage/bemvindo";
-import CombosExclusivos from "@/components/sections/homepage/combos";
-import TransformeCarreira from "@/components/sections/homepage/paixao";
-import FormacaoTecnica from "@/components/sections/homepage/formacao";
-import Blog from '@/components/sections/homepage/blog';
-import EduOn from '@/components/sections/homepage/eduon';
-import Localizacao from '@/components/sections/homepage/localizacao';
 import Footer from '@/components/footer/footer';
+import CombosExclusivos from '@/components/sections/homepage/combos';
+
+const LazyComponent = ({ component: Component }: { component: React.LazyExoticComponent<React.ComponentType<any>> }) => {
+    const { ref, inView } = useInView({ triggerOnce: true });
+
+    return (
+        <div ref={ref}>
+            {inView && (
+                <Suspense fallback={<div>Carregando...</div>}>
+                    <Component />
+                </Suspense>
+            )}
+        </div>
+    );
+};
+
+const LazyTransformeCarreira = React.lazy(() => import('@/components/sections/homepage/paixao'));
+const LazyCombosExclusivosWrapper = React.lazy(() =>
+    Promise.resolve({
+        default: () => (
+            <CombosExclusivos title={<>Conheça nossos <span className="text-primary">combos exclusivos</span></>} buttonText={"Conheça outros cursos"} />
+        )
+    })
+);
+const LazyFormacaoTecnica = React.lazy(() => import('@/components/sections/homepage/formacao'));
+const LazyBlog = React.lazy(() => import('@/components/sections/homepage/blog'));
+const LazyEduOn = React.lazy(() => import('@/components/sections/homepage/eduon'));
+const LazyLocalizacao = React.lazy(() => import('@/components/sections/homepage/localizacao'));
+
 const HomePage: React.FC = () => {
     const Titulo = <>Conheça nossos <span className="text-primary">combos exclusivos</span></>;
     const buttonTexto = 'Conheça outros cursos';
+
     return (
-        <main >
+        <main>
             <SejaBemVindo />
 
-            <TransformeCarreira />
+            <LazyComponent component={LazyTransformeCarreira} />
+            <LazyComponent component={LazyCombosExclusivosWrapper} />
+            <LazyComponent component={LazyFormacaoTecnica} />
+            <LazyComponent component={LazyBlog} />
+            <LazyComponent component={LazyEduOn} />
+            <LazyComponent component={LazyLocalizacao} />
 
-            <CombosExclusivos title={Titulo} buttonText={buttonTexto} />
-
-            <FormacaoTecnica />
-            <Blog />
-            <EduOn />
-            <Localizacao />
             <Footer />
-
-            {/*  */}
-
-        </main >
+        </main>
     );
-}
+};
 
 export default React.memo(HomePage);
